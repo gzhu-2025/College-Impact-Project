ba8ececdb8721a44fb4c1c9f8c0b2552742dfc64
gzhu2025 <67470875+gzhu-2025@users.noreply.github.com>
2024-06-30 16:03:08 -0400

    Add files via upload

---
+++ rename.py
@@ -0,0 +1,7 @@
+import os
+
+target_dir = 'data/labels/Crosswalk'
+
+for filename in os.listdir(target_dir):
+    newname = "Crosswalk " + filename.split()[-1]
+    os.rename(os.path.join(target_dir, filename), os.path.join(target_dir, newname))
