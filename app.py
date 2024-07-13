78c0142d2f567aa03f4132ee06567559cb9de37a
gzhu2025 <67470875+gzhu-2025@users.noreply.github.com>
2024-07-13 09:42:43 +0000

    deleted unnecessary comments differentiated ToTensor transforms for training and testing created Dataset class for images modified batch display function to work with both testing and training mode saved trained model with <0.01 avg loss loss plotting modified testing function

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 import torch, os, sys
 
 from skimage import io, transform
@@ -14,31 +16,6 @@ import warnings
 warnings.filterwarnings("ignore")
 
 plt.ion()
-
-# if (sys.argv[1] != "Chimney"
-#     and sys.argv[1] != "Crosswalk"
-#     and sys.argv[1] != "Stair"
-#     ):
-#     raise Exception("Labels not found")
-
-# label_path = f"data/labels/_ndarrays/{sys.argv[1]}.npy"
-
-# n = 37
-
-# grid = np.load(label_path)[n - 1]
-
-# # print(f"grid {grid[0]}: ")
-# n = grid[0]
-
-# grid = grid[1:]
-
-# image_name = f"data/images/{sys.argv[1]}/{'Cross' if sys.argv[1] == 'Crosswalk' else sys.argv[1]} ({n}).png"
-
-# # for i in range(4):
-#     for j in range(4):
-#         print(grid[i * 4 + j], end=" ")
-#     print()
-
 
 def show_image(image, grid):
     fig = plt.gcf()
@@ -68,9 +45,10 @@ def show_image(image, grid):
             ax.add_patch(rect)
 
 
-class ToTensor(object):
+class ToTensor_train(object):
 
     def __call__(self, sample):
+       
         image, grid = sample["image"], sample["grid"]
 
         image = image.transpose((2, 0, 1))
@@ -79,19 +57,22 @@ class ToTensor(object):
             "image": torch.from_numpy(image) / 255.0,
             "grid": torch.from_numpy(grid),
         }
+    
 
 
-# plt.figure()
-# # print(image_name)
-# show_image(image_name, io.imread(image_name))
+class ToTensor_test(object):
 
-# plt.show(block=True)
+    def __call__(self, sample):
+    
+        image = sample["image"]
+
+        image = image.transpose((2, 0, 1))
+        return {
+            "image_name": sample["image_name"],
+            "image": torch.from_numpy(image) / 255.0,
+        }
 
 
-# gridpoints_dataset = GridPointsDataset(category='Crosswalk', array_dir='data/labels/_ndarrays', image_dir='data/images', transform=transforms.Compose([
-#                                                                                                                                         ToTensor()
-#                                                                                                                                     ]))
-# dataloader = DataLoader(gridpoints_dataset, batch_size=4, shuffle=True, num_workers=0)
 class GridPointsDataset(Dataset):
 
     def __init__(self, category, array_dir, image_dir, transform=None):
@@ -106,7 +87,7 @@ class GridPointsDataset(Dataset):
         self.transform = transform
 
     def __len__(self):
-        return gridpoints_dataset.grids.shape[0]
+        return self.grids.shape[0]
 
     def __getitem__(self, index):
         if torch.is_tensor(index):
@@ -131,50 +112,52 @@ class GridPointsDataset(Dataset):
 
         return sample
 
+class ImageDataset(Dataset):
 
-# training_data = datasets.FashionMNIST(
-#     root="data",
-#     train=True,
-#     download=True,
-#     transform=ToTensor(),
-# )
+    def __init__(self, category, image_dir, transform=None):
 
-# test_data = datasets.FashionMNIST(
-#     root="data",
-#     train=False,
-#     download=True,
-#     transform=ToTensor(),
-# )
+        self.category = category  # Crosswalk, Chimney, Stair
 
-# gridpoints_dataset = GridPointsDataset(category='Crosswalk', array_dir='data/labels/_ndarrays', image_dir='data/images', transform=None)
-# fig = plt.figure()
+        self.image_dir = image_dir
+        self.transform = transform
 
-# for i, sample in enumerate(gridpoints_dataset):
-#     print(i, sample['image'].shape, sample['grid'].shape)
+    def __len__(self):
+        return len(os.listdir(self.image_dir))
 
-#     ax = plt.subplot(1, 4, i + 1)
-#     plt.tight_layout()
-#     ax.set_title(f'Sample #{i}, {sample['image_name']}')
-#     ax.axis('off')
-#     show_image(**sample)
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            index = index.toList()
 
-#     if i==3:
-#         plt.show(block=True)
-#         break
-#
+        image_path = os.path.normpath(
+            os.path.join(
+                self.image_dir,
+                self.category,
+            )
+        )
 
-# gridpoints_dataset = GridPointsDataset(category='Crosswalk', array_dir='data/labels/_ndarrays', image_dir='data/images', transform=transforms.Compose([
-#                                                                                                                                         ToTensor()
-#                                                                                                                                     ]))
-# for i, sample in enumerate(gridpoints_dataset):
-#     print(i, sample['image'].size(), sample['grid'].size())
+        image_name = os.listdir(image_path)[index]
 
-#     if i==3:
-#         break
+        image_name = os.path.normpath(
+            os.path.join(
+                image_path, 
+                image_name,
+            )
+        )
 
+        image = io.imread(image_name)
 
-def show_grid_batch(sample_batched):
-    images_batch, grid_batch = sample_batched["image"], sample_batched["grid"]
+        sample = {"image_name": image_name, "image": image}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+def show_grid_batch(sample_batched, train):
+    print(f"train: {train}")
+    images_batch = sample_batched["image"]
+    if train: grid_batch =  sample_batched["grid"]
+
     batch_size = len(images_batch)
     im_size = images_batch.size(2)
     image_grid_border_size = 0
@@ -188,60 +171,63 @@ def show_grid_batch(sample_batched):
 
     ax = plt.gca()
 
-    for i in range(batch_size):
-        for j in range(16):
+    if train:
+        for i in range(batch_size):
+            for j in range(16):
 
-            if grid_batch[i][j].item():
-                row = int(j / 4)
-                col = j % 4
+                if grid_batch[i][j].item():
+                    row = int(j / 4)
+                    col = j % 4
 
-                rect = patches.Rectangle(
-                    (col * 30 + 123 * i, row * 30),
-                    30,
-                    30,
-                    linewidth=1,
-                    edgecolor="#1a73e8",
-                    facecolor="#1a73e8",
-                    alpha=0.3,
-                )
-                ax.add_patch(rect)
+                    rect = patches.Rectangle(
+                        (col * 30 + 123 * i, row * 30),
+                        30,
+                        30,
+                        linewidth=1,
+                        edgecolor="#1a73e8",
+                        facecolor="#1a73e8",
+                        alpha=0.3,
+                    )
+                    ax.add_patch(rect)
 
-        plt.title("Batch from dataloader")
+    plt.title("Batch from dataloader")
 
+def show_batch(dataloader, train):
+    if __name__ == "__main__":
+        for i_batch, sample_batched in enumerate(dataloader):
+            if train: 
+                print("train", i_batch, sample_batched["image"].size(), sample_batched["grid"].size())
+            else: 
+                print("test", i_batch, sample_batched["image"].size())
+
+            if i_batch == 3 or sample_batched != False:
+                plt.figure()
+                show_grid_batch(sample_batched, train)
+                plt.title(f"Train: {train}")
+                plt.axis("off")
+                plt.ioff()
+                plt.show(block=True)
+                break
 
 gridpoints_dataset = GridPointsDataset(
     category="Crosswalk",
     array_dir="data/labels/_ndarrays",
     image_dir="data/images",
-    transform=transforms.Compose([ToTensor()]),
+    transform=transforms.Compose([ToTensor_train()]),
 )
-dataloader = DataLoader(gridpoints_dataset, batch_size=4, shuffle=True, num_workers=0)
 
-if __name__ == "__main__":
-    for i_batch, sample_batched in enumerate(dataloader):
-        print(i_batch, sample_batched["image"].size(), sample_batched["grid"].size())
+image_dataset = ImageDataset(
+    category="Crosswalk",
+    image_dir="data/images",
+    transform=transforms.Compose([ToTensor_test()]),
+)
 
-        if i_batch == 3:
-            plt.figure()
-            show_grid_batch(sample_batched)
-            plt.axis("off")
-            plt.ioff()
-            plt.show(block=True)
-            break
+training_dataloader = DataLoader(gridpoints_dataset, batch_size=4, shuffle=True, num_workers=0)
+test_dataloader = DataLoader(image_dataset, batch_size=4, shuffle=True, num_workers=0)
 
-# batch_size = 64
-
-# train_dataloader = DataLoader(training_data, batch_size=batch_size)
-# test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
-# for X, y in test_dataloader:
-#     print(f"Shape of X [N, C, H, W]: {X.shape}")
-#     print(f"Shape of y: {y.shape} {y.dtype}")
-#     break
-
-# for i_batch, sample_batched in enumerate(dataloader):
-#     print(i_batch, sample_batched['image'].size(), sample_batched['image'].dtype,
-#             sample_batched['grid'].size(), sample_batched['grid'].dtype)
+   
+show_batch(training_dataloader, train=True)
+show_batch(test_dataloader, train=False)
 
 device = (
     "cuda"
@@ -257,11 +243,11 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(120 * 120 * 4, 512),
+            nn.Linear(120 * 120 * 4, 256),
             nn.ReLU(),
-            nn.Linear(512, 512),
+            nn.Linear(256, 256),
             nn.ReLU(),
-            nn.Linear(512, 16),
+            nn.Linear(256, 16),
         )
 
     def forward(self, x):
@@ -270,34 +256,60 @@ class NeuralNetwork(nn.Module):
         return logits
 
 
-model = NeuralNetwork().to(device)
+# model = NeuralNetwork().to(device)
 # print(model)
 
 # loss_fn = nn.MSELoss()
 loss_fn = nn.BCEWithLogitsLoss()
 # optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+lr = 1e-4
+
+# optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
 
 def train(dataloader, model, loss_fn, optimizer, epochs=100):
+    # scheduled_lr = 0
+    # ideal start: 4e-3
+
     size = len(dataloader.dataset)
     model.train()
     losses = []
+    lossavg= []
+    epochavg = []
     for epoch in range(epochs):
+        
+        # if epoch % 10 == 0: scheduled_lr += 1e-7
+        # ideal end: 2e-2
         for batch, sample in enumerate(dataloader):
+
             image, grid = sample["image"].to(device), sample["grid"].to(device)
 
             pred = model(image)
-            loss = loss_fn(pred, grid.type(torch.float32))  #
+            loss = loss_fn(pred, grid.type(torch.float32))  
 
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
             losses.append(loss.item())
             if batch % 10 == 0:
+                os.system('cls')
+                print("epoch: ", epoch)
                 loss, current = np.mean(losses[-50:]), (batch + 1) * len(image)
-                print(f"loss: {loss:>f} [{current:>5d}/{size:>5d}]")
+                lossavg.append(loss)
+                epochavg.append(epoch)
+                print(f"loss: {loss:>f} [{current:>5d}/{size:>5d}]\nlr: {lr}")
+    
+    plot_loss(losses, epochs, lossavg, epochavg)
 
+def plot_loss(loss, epochs, lossavg, epochavg):
+    plt.figure()
+    plt.ioff()
+    plt.plot(loss)
+    plt.plot(epochavg, lossavg)
+    plt.axis((0, epochs, 0, max(loss)))
+    # print(max(loss))
+    plt.show(block=True)
+            
 
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
@@ -309,7 +321,8 @@ def test(dataloader, model, loss_fn):
             image, grid = sample["image"].to(device), sample["grid"].to(device)
 
             pred = model(image)
-            test_loss += loss_fn(pred, grid).item()
+            print(pred)
+            test_loss += loss_fn(torch.round(pred), torch.round(grid)).item()
             print(pred, grid)
             correct += (pred.argmax(1) == grid).type(torch.float).sum().item()
     test_loss /= num_batches
@@ -319,31 +332,16 @@ def test(dataloader, model, loss_fn):
     )
 
 
-# epochs = 5
-# for t in range(epochs):
-#     print(f"Epoch {t + 1}\n----------------------------------------")
-#     train(dataloader, model, loss_fn, optimizer)
-#     test(dataloader, model, loss_fn)
-# print("Done!")
+
+# train(training_dataloader, model, loss_fn, optimizer, epochs=250)
 
 # torch.save(model.state_dict(), "model.pth")
 # print("Saved PyTorch Model State to model.pth")
 
-# model = NeuralNetwork().to(device)
-# model.load_state_dict(torch.load("model.pth"))
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load("model.pth"))
 
-# classes = [
-#     "T-shirt/top",
-#     "Trouser",
-#     "Pullover",
-#     "Dress",
-#     "Coat",
-#     "Sandal",
-#     "Shirt",
-#     "Sneaker",
-#     "Bag",
-#     "Ankle boot",
-# ]
+test(training_dataloader, model, loss_fn)
 
 # model.eval()
 # x, y = test_data[0][0], test_data[0][1]
