e4f915f8e03f5d03850d330364bffcc3201724e3
gzhu2025 <67470875+gzhu-2025@users.noreply.github.com>
2024-08-11 10:02:08 -0700

    fixed logic error

--- scrape.py
+++ scrape.py
@@ -1,9 +1,9 @@
-import os, request
+import os, requests
 
 key = 'AIzaSyC2CxKExB2dVr9ISnz4sFe3mq379DV7lEo'
 cx = '33a40a6ede1304964'
-uri = 'https://www.googleapis.com/customsearch/'
+uri = 'https://www.googleapis.com/customsearch/v1'
 
-requests = request.get(uri, params={'key': key, 'cx': cx, 'searchType': 'image', 'q': 'Car'})
+results = requests.get(uri, params={'key': key, 'cx': cx, 'searchType': 'image', 'q': 'Car'})
 
-print(requests.json())
+print(results.json())
