61ccb88b2680f1c6634921720ba9d3d916ab2da5
gzhu2025 <67470875+gzhu-2025@users.noreply.github.com>
2024-08-13 06:05:27 -0700

    added more scraping functionality

--- scrape.py
+++ scrape.py
@@ -4,6 +4,37 @@ key = 'AIzaSyC2CxKExB2dVr9ISnz4sFe3mq379DV7lEo'
 cx = '33a40a6ede1304964'
 uri = 'https://www.googleapis.com/customsearch/v1'
 
-results = requests.get(uri, params={'key': key, 'cx': cx, 'searchType': 'image', 'q': 'Car'})
+searchTerms = ['Bicycle', 
+               'Bridge', 
+               'Bus', 
+               'Car', 
+               'Chimney', 
+               'Crosswalk', 
+               'Hydrant', 
+               'Motorcycle', 
+               'Palm', 
+               'Stair', 
+               'Traffic Light', 
+               ]
+for searchTerm in searchTerms:
+    directory = os.path.join('data', searchTerm)
+    
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    
+    results = requests.get(uri, params={'key': key, 'cx': cx, 'searchType': 'image', 'fileType': 'png', 'q': searchTerm})
 
-print(results.json())
+    for result in results.json()['items']:
+        i = 1
+        # print(f"{result}:", results.json()[result])
+        # print(f"{result}:", results.json()['items'][0][result])
+        # print(result['link'])
+        filename = f'{searchTerm}_{i}.png'
+        filepath = os.path.join(directory, filename)
+
+        image = requests.get(result['link'])
+        print(image.ok, image.status_code)
+        print(result['link'])
+        if image.ok or int(image.status_code) == 200:
+            with open(filepath, mode='wb') as file:
+                file.write(image.content)
